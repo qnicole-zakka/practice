d76ae1cc79f49850340c0f773c24bc6b4e7efe64
qnicole-zakka <qnicole87@yahoo.com>
2019-09-06 16:05:59 -0700

    understand logits and probabilities in prediction

---
+++ tfPred.py
@@ -0,0 +1,23 @@
+import input_data
+import tensorflow as tf
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+x = tf.placeholder("float", shape=[None, 784])
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+y_ = tf.placeholder("float", shape=[None, 10])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+# train data and get results for batches
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+# train the data
+for i in range(10):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print("accuracy", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+prediction=tf.argmax(y,1)
+print("predictions", prediction.eval(feed_dict={x: mnist.test.images}, session=sess))
